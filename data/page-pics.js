a85f42723ff839d07ec3dbebc64e13739cfea05d
rodk <rod2010rod2010@hotmail.com>
2016-11-29 00:46:07 -0500

    cleaned up some more

--- data/page-pics.js
+++ data/page-pics.js
@@ -2,12 +2,5 @@ import home from '../public/images/others/home.jpg'
 import about from '../public/images/others/about.jpeg'
 
 export default function() {
-  return [
-    {
-      homeImg: home
-    },
-    {
-      aboutImg: about
-    }
-  ];
+  return [{homeImg: home}, {aboutImg: about}];
 };
