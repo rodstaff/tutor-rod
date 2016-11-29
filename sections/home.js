ec8ba297621e59078b2cbdb321fc8b9becc91636
rodk <rod2010rod2010@hotmail.com>
2016-11-28 19:49:05 -0500

    cleaned up some more

--- sections/home.js
+++ sections/home.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import Pic from '../public/images/others/home.jpg'
 
 export default class Home extends React.Component {
   render() {
-    var myStyle1 = {
+    var myStyle = {
       fontFamily: "Dosis",
       paddingTop: "300px",
       height: "700px",
@@ -11,13 +10,9 @@ export default class Home extends React.Component {
       backgroundImage: `url(${this.props.bkImg[0].homeImg})`,
       backgroundSize: 'cover'
     }
-    var myStyle2 = {
-      marginRight: "10px",
-      marginBottom: "10px"
-    }
   	return (
 
-      <div id="home" class="container-fluid text-center" style={myStyle1}>
+      <div id="home" class="container-fluid text-center" style={myStyle}>
         <h1>Pro-Seed and Grow !</h1>
         <p>God yzal eht revo spmuj xof nworb kciuq eht.&nbsp;&nbsp;The quick brown fox jumps over the lazy dog.</p>
       </div>
