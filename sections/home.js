0c9b046267b3cb33a1aa796aa813c9daec86a0bb
rodk <rod2010rod2010@hotmail.com>
2017-03-30 00:25:50 -0400

    modified sections/home.js

--- sections/home.js
+++ sections/home.js
@@ -27,8 +27,8 @@ export default class Home extends React.Component {
       
       <div id="home" class="container-fluid text-center" style={myStyle1}>
         <header style={myStyle3}>Math, Science, Test Prep Tutors Here!</header>
-        <h1 style={myStyle4}>Technology is being used in education in ways that help facilitate learning.  
-        for example, online platforms are being used to reach out to more people liberating both the tutor and student constraints of space and time.  However, a number of tutors still cling to the traditional face-to-face approach and for a reason...</h1>
+        <h1 style={myStyle4}>Technology is now being used in education in ways that help facilitate learning.  
+        for example, online platforms are being used to reach out to more people liberating both the tutor and student constraints of space and time.  Yet, owever, a number of tutors still cling to the traditional face-to-face approach and you may be wondering why...</h1>
         <h2>{'<'}<em> Only warm, dedicated tutors here </em>{'>'}&nbsp;</h2>
         <div style={myStyle2}>
           <p>{this.props.bkImg[0].size}</p>
