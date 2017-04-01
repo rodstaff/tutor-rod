d3c27e87b12e57c043576319008d2ca78888087a
rodk <rod2010rod2010@hotmail.com>
2017-04-01 17:10:09 -0400

    changed home background image

--- data/page-pics.js
+++ data/page-pics.js
@@ -1,4 +1,4 @@
-import home from '../public/images/others/blackboard2.png'
+import home from '../public/images/others/emptyblckboard1.jpg'
 import about from '../public/images/others/booksboard.jpg'
 
 export default function() {
