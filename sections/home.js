b80611d6aaa9ed88d479257ad536acf86203b940
rodk <rod2010rod2010@hotmail.com>
2017-04-03 14:39:08 -0400

    still trying other fonts, wordings in home page

--- sections/home.js
+++ sections/home.js
@@ -6,8 +6,8 @@ export default class Home extends React.Component {
       fontFamily: "Dancing Script",
       paddingTop: "170px",
       height: "930px",
-      color: "#00FFFF",
-      //color:  "#fff",
+      // color: "#00FFFF",
+      color:  "#fff",
       backgroundImage: `url(${this.props.bkImg[0].homeImg})`,
       backgroundSize: 'cover'
     }
@@ -19,17 +19,17 @@ export default class Home extends React.Component {
       fontSize: 100
     }
     var myStyle4 = {
+      paddingTop: "130px",
       fontSize: 40,
-      fontFamily:  "Dosis",
+      fontFamily:  "Macondo",
       color: "#fff"
     }
   	return (
       
       <div id="home" class="container-fluid text-center" style={myStyle1}>
-        <header style={myStyle3}>Math, Science, Test Prep Tutors Here!</header>
+        <header style={myStyle3}>Math, Science, Test Prep ... Great Tutors Here, Look No Further!</header>
         <h1 style={myStyle4}>Technology is now being used in education in ways that help facilitate learning.  
-        for example, online platforms are being used to reach out to more people liberating both the tutor and student constraints of space and time.  Yet, owever, a number of tutors still cling to the traditional face-to-face approach and you may be wondering why...</h1>
-        <h2>{'<'}<em> Only warm, dedicated tutors here </em>{'>'}&nbsp;</h2>
+        For example, online platforms are being used to reach out to more people liberating both the tutor and student constraints of space and time.  Yet, however, a number of tutors still cling to the traditional face-to-face approach and you may be wondering why...</h1>
         <div style={myStyle2}>
           <p>{this.props.bkImg[0].size}</p>
         </div>
