05f0b26855c33b1e88441d03a47b3579c7c303f5
rodk <rod2010rod2010@hotmail.com>
2017-04-03 14:51:33 -0400

    modified home page text content

--- sections/home.js
+++ sections/home.js
@@ -28,8 +28,8 @@ export default class Home extends React.Component {
       
       <div id="home" class="container-fluid text-center" style={myStyle1}>
         <header style={myStyle3}>Math, Science, Test Prep ... Great Tutors Here, Look No Further!</header>
-        <h1 style={myStyle4}>Technology is now being used in education in ways that help facilitate learning.  
-        For example, online platforms are being used to reach out to more people liberating both the tutor and student constraints of space and time.  Yet, however, a number of tutors still cling to the traditional face-to-face approach and you may be wondering why...</h1>
+        <h1 style={myStyle4}>Technology is now being used in education in ways that help facilitate learning.&nbsp;  
+        For example, online platforms are being used to reach out to more people liberating both the tutor and student constraints of space and time.&nbsp; Yet, however, a number of tutors still cling to the traditional face-to-face approach and you may be wondering why...</h1>
         <div style={myStyle2}>
           <p>{this.props.bkImg[0].size}</p>
         </div>
