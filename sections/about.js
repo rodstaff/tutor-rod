e89aa28c82c64ca9f106547f1b560cdd0e67d705
rodk <rod2010rod2010@hotmail.com>
2017-04-22 00:06:05 -0400

    modified about text

--- sections/about.js
+++ sections/about.js
@@ -14,22 +14,29 @@ export default class About extends React.Component {
       backgroundSize: 'cover'
     }
     var myStyle2 = {
+    paddingTop: "170px",
     // marginRight: "700px",
     // marginBottom: "30px",
-    // marginLeft: "30px",
+    marginLeft: "10px",
     // height: "680px",
-      fontSize: 57
+      fontSize: 57,
+      color: "yellow"
     }
     var myStyle3 = {
       color: "orange",
+      marginLeft: "10px"
+    }
+    var myStyle4 = {
+      fontSize: 40,
+      color: "yellow",
       marginLeft: "10px"
     }
   	return (
 
       <div id="about" class="container-fluid text-left" style={myStyle1}>
         <h1 style={myStyle2}>Welcome to Octonics Learning!</h1>
-        <h1 style={myStyle2}>Here we value the human interaction versus the more impersonal online tutoring alternative.</h1>
-        <h2> Our tutors come from elite schools and good scholastic aptitudes </h2>
+        <h1 style={myStyle4}>Here we value the human interaction versus the more impersonal online tutoring alternative.</h1>
+        <h2 style={myStyle4}> Our tutors come from elite schools and have impressive scholastic achievements.</h2>
         <p class="text-left" style={myStyle3}>&emsp;&ensp;{this.props.bkImg[1].size}</p>
       </div>
     );
