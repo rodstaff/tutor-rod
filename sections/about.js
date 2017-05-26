b5ddf0eea3b92a69c463a8a1c762c979e6beb1d6
rodk <rod2010rod2010@hotmail.com>
2017-05-26 15:42:00 -0400

    updated home, about pages text

--- sections/about.js
+++ sections/about.js
@@ -35,7 +35,6 @@ export default class About extends React.Component {
 
       <div id="about" class="container-fluid text-left" style={myStyle1}>
         <h1 style={myStyle2}>Welcome to Octonics Mentoring!</h1>
-        <h1 style={myStyle4}>Here we value the human interaction versus the more impersonal online tutoring alternative.</h1>
         <h2 style={myStyle4}> Our tutors come from elite schools and have impressive scholastic achievements. More importantly, our tutors have that extra patience and creativity to enrich your learning experience.  </h2>
         <h2 style={myStyle4}> Contact us! </h2>
         <p class="text-left" style={myStyle3}>&emsp;&ensp;{this.props.bkImg[1].size}</p>
