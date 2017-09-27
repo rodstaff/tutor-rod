3e40e56cbf36bc6faaa53dc0d82b1f19b0c606c3
rodk <rod2010rod2010@hotmail.com>
2017-09-26 22:38:03 -0400

    modified about text

--- sections/about.js
+++ sections/about.js
@@ -34,7 +34,7 @@ export default class About extends React.Component {
   	return (
 
       <div id="about" class="container-fluid text-left" style={myStyle1}>
-        <h1 style={myStyle2}>Introducing Immersive Courses for Smarts Here at NYC!</h1>
+        <h1 style={myStyle2}>Introducing Immersive Courses for Smarts at NYC!</h1>
         <h2 style={myStyle4}> We specialize in standardized testing and Math tutoring.  </h2>
         <p class="text-left" style={myStyle3}>&emsp;&ensp;{this.props.bkImg[1].size}</p>
       </div>
